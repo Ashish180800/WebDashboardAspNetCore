d731155f119f38c22e04253c40afcd5ae414b534
Ashish Sharma <140501108+Ashish180800@users.noreply.github.com>
2023-08-17 07:39:13 +0530

    Add project files.

---
+++ asp-net-core-dashboard-project/Controllers/DefaultDashboardController.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardWeb;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace asp_net_core_dashboard_project {
+    public class DefaultDashboardController : DashboardController {
+        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider dataProtectionProvider = null)
+            : base(configurator, dataProtectionProvider) {
+        }
+    }
+}
